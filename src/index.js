ad31babe4551167590602b10f8ad0d6e6e532615
RAUNAK ROY BARMAN <48653401+RRBarman@users.noreply.github.com>
2023-01-19 15:45:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-
+class API {
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    #secure = url.startsWith("https") ? true : false;
+  }
+  isSecure() {
+    return this.#secure;
+  }
+  updateURL(newUrl) {
+    this.url = newUrl;
+    this.#secure = newUrl.startsWith("https") ? true : false;
+  }
+}
 
 module.exports = { API }
